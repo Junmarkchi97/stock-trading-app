60e96121f62494eb3450a8ba9ea75a9ea8f462b3
Junmark Chi <chijunmark97@gmail.com>
2022-08-31 05:45:49 +0800

    Add login stylesheets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.sass.preferred_syntax = :scss
+  config.sass.line_comments = false
+  config.sass.cache = false
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
