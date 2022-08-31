e31b8cdd628c5e54a66f7b87f029e10cc656fe79
Franco Rivera <franco@LAPTOP-7R15A7O9.localdomain>
2022-08-31 21:40:30 +0800

    Add IEX client

---
+++ config/initializers/iex_client.rb
@@ -0,0 +1,5 @@
+# IEX::Api.configure do |config|
+#     config.publishable_token = 'pk_4b5edcd3a6884cf08b0588217f3fab8a' # defaults to ENV['IEX_API_PUBLISHABLE_TOKEN']
+#     config.secret_token = 'sk_d6d8de248d1748b4a3e1fa3ff4db1c24' # defaults to ENV['IEX_API_SECRET_TOKEN']
+#     config.endpoint = 'https://cloud.iexapis.com/v1' # use 'https://sandbox.iexapis.com/v1' for Sandbox
+#   end
